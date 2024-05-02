5fa1817e5c44f590730ccccfbf78c17031cc631b
Emmanuele <emagnubcp@gmail.com>
2024-05-02 22:32:05 +0100

    Starting code - 152. Module Introduction, 153. Project Setup

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+//  //   ///
+//  Import LIBRARIES
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+//  Import FIILES
+//  PARTS
+//  PROVIDERS
+//  //   ///
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meal app',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: const Color.fromARGB(255, 131, 57, 0),
+        ),
+        textTheme: GoogleFonts.latoTextTheme(),
+        useMaterial3: true,
+      ),
+      // home: // Todo ...,
+    );
+  }
+}
+
+
+
+
+
+//  //   ///
+//  Import LIBRARIES
+//  Import FIILES
+//  PARTS
+//  PROVIDERS
+//  //   ///
