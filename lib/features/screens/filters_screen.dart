61c16fa36bfe94eb9c1fead708930d047a09149a
Emmanuele <emagnubcp@gmail.com>
2024-05-05 00:06:21 +0100

    171. Adding a Filter Item

---
+++ lib/features/screens/filters_screen.dart
@@ -0,0 +1,47 @@
+//  //   ///
+//  Import LIBRARIES
+import 'package:flutter/material.dart';
+//  Import FILES
+//  PARTS
+//  PROVIDERS
+//  //   ///
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      body: Column(
+        children: <Widget>[
+          SwitchListTile(
+            value: value,
+            onChanged: onChanged,
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include gluten-free meals.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+        ],
+      ),
+    );
+  }
+}
